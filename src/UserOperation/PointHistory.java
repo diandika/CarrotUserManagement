e3e2825f0ce91677c3f72d0219f9622af93385bb
D Andika Putra <mohammaddicky.andika@gmail.com>
2022-03-01 15:55:30 +0700

    modify class content

--- src/UserOperation/PointHistory.java
+++ src/UserOperation/PointHistory.java
@@ -1,23 +1,29 @@
 package UserOperation;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class PointHistory {
-    private String changeType; // "Get" or "Send"
-    private User fromOrTo;
+    private Timestamp timestamp;
+    private User senderUser;
+    private User recipientUser;
     private int point;
 
     /** create new history log
      *
-     * @param changeType
-     * @param fromOrTo
+     * @param recipientUser
+     * @param senderUser
      * @param point
      */
-    public PointHistory(String changeType, User fromOrTo, int point){
-
+    public PointHistory(User senderUser, User recipientUser, int point){
+        this.timestamp = new Timestamp(new Date().getTime());
+        this.senderUser = senderUser;
+        this.recipientUser = recipientUser;
+        this.point = point;
     }
 
     public String toString(){
 
-
-        return "string";
+        return this.timestamp + ":: " + senderUser.getName() + " send " + this.point + " carrots to " + recipientUser.getName();
     }
 }
